0ef3b31262881fe2903528190439220045c3b800
bernardoct <bct52@cornell.edu>
2018-06-20 09:51:39 -0400

    Update ReservoirMassBalance1.cpp
    Random function changed in HW 1.

--- HW1/ReservoirMassBalance1.cpp
+++ HW1/ReservoirMassBalance1.cpp
@@ -11,7 +11,6 @@ int main() {
     const double CAPACITY = 100.;
     const double MIN_ENV_FLOW = 5.;
     const double MAX_INFLOW_EVAP = 10.;
-    const double MIN_INFLOW_EVAP = 0.;
 
     // Declare state and time series variables
     double volumes[NUMBER_OF_RESERVOIRS];
@@ -19,15 +18,11 @@ int main() {
     double catchment_inflows[NUMBER_OF_RESERVOIRS][NUMBER_OF_TIME_STEPS];
     double evaporations[NUMBER_OF_RESERVOIRS][NUMBER_OF_TIME_STEPS];
 
-    // Create uniform distribution for inflow and evaporation sampling
-    std::uniform_real_distribution<double> unif(MIN_INFLOW_EVAP, MAX_INFLOW_EVAP);
-    std::default_random_engine re;
-
     // Populate inflows and evaporation vectors with random values, volumes with capacities, and min env flows.
     for (int r = 0; r < NUMBER_OF_RESERVOIRS; ++r) {
         for (int t = 0; t < NUMBER_OF_TIME_STEPS; ++t) {
-            catchment_inflows[r][t] = unif(re);
-            evaporations[r][t] = unif(re);
+            catchment_inflows[r][t] = 1. * rand() / RAND_MAX * MAX_INFLOW_EVAP;
+            evaporations[r][t] = 1. * rand() / RAND_MAX * MAX_INFLOW_EVAP;
         }
         volumes[r] = CAPACITY;
         min_env_flows[r] = MIN_ENV_FLOW;
@@ -43,16 +38,19 @@ int main() {
 
             // Perform mass balance
             volumes[r] += release + catchment_inflows[r][t] - evaporations[r][t] - min_env_flows[r];
+            double upstream_release = release;
+            release = min_env_flows[r];
             if (volumes[r] > CAPACITY) {
                 release += volumes[r] - CAPACITY;
                 volumes[r] = CAPACITY;
-            } else if (volumes[r] < 0) {
-                release = min_env_flows[r] + volumes[r];
-                volumes[r] = 0;
+            } else if (volumes[r] < 0.) {
+                release = max(min_env_flows[r] + volumes[r], 0.);
+                volumes[r] = 0.;
             } else {
                 release = min_env_flows[r];
             }
-            std::cout << volumes[r] << " ";
+            std::cout << volumes[r] << " " << upstream_release << " " << catchment_inflows[r][t] << " "
+                      << evaporations[r][t] << " " << release << "         ";
         }
         std::cout << std::endl;
     }
